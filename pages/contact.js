68b8f0761e6f263b17b1eeddb4e55fc7f7381940
arunmani <marun1156@gmail.com>
2022-03-09 08:59:57 +0530

    first commit

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+
+export default function Contact(){
+    return <div className={styles.newDiv}>
+        <p>Home</p>
+        <p>Contact</p>
+        <p>Login</p>
+    </div>
+}
